92ff042ba788f1d8ffa75a21f810b741bb3805e8
shubhada yadav <shubhada.yadav107@gmail.com>
2026-05-27 19:07:02 +0530

    About Section created

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 new Swiper(".menu-scroll", {
-  // Optional parameters
-  slidesPerView: 3,
-  spaceBetween: 30,
-  //   loop: true,
+    // Optional parameters
+    slidesPerView: 3,
+    spaceBetween: 30,
+    //   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
