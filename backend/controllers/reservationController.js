6d70cc65f57abb2739000291e6d1f6e8365f0b05
Shubhada Yadav <shubhada.yadav107@gmail.com>
2026-09-23 23:56:29 +0530

    Add reservation form validation and duplicate error handling

--- backend/controllers/reservationController.js
+++ backend/controllers/reservationController.js
@@ -3,9 +3,29 @@ const Reservation = require("../model/reservationModels");
 const createReservation = async (req, res) => {
   console.log("Controller called");
   console.log("Received data:", req.body);
+  try {
+    const response = await Reservation.create(req.body);
 
-  const response = await Reservation.create(req.body);
-  res.json({ message: " Reservation Successfull", reservation: response });
+    res
+      .status(201)
+      .json({ message: " Reservation Successfull", reservation: response });
+  } catch (error) {
+    // Check if Email Id and phone number is Duplicate
+    if (error.code === 11000) {
+      if (error.keyValue.emailId) {
+        return res.status(400).json({
+          field: "emailId",
+          message: " This Email Id is already registered",
+        });
+      }
+      if (error.keyValue.telNumber) {
+        return res.status(400).json({
+          field: "telNumber",
+          message: "This Phone Number is already registered ",
+        });
+      }
+    }
+  }
 };
 
 module.exports = { createReservation };
--- frontend/index.js
+++ frontend/index.js
@@ -23,13 +23,95 @@ new Swiper(".menu-scroll", {
 let form = document.getElementById("reservationForm");
 let successPopup = document.getElementById("successPopup");
 const closePopup = document.getElementById("closePopup");
-
 const backHomeBtn = document.getElementById("backHomeBtn");
+const selectDate = document.getElementById("user_selectDate");
+
+const today = new Date().toISOString().split("T")[0];
+// toISOString().split("T")[0]   //this is format to convert date in yyyy-mm-dd
+// selectDate.min = today;
+selectDate.setAttribute("min", today);
+
+//when errror msg appears and we type in input field , error msg will disappear
+const nameError = document.getElementById("nameError");
+const nameInput = document.getElementById("name");
+nameInput.addEventListener("input", () => {
+  if (nameInput.value.trim() !== " ") {
+    // alert(" Please Enter The Name");
+    nameError.textContent = " ";
+    // return false;
+  }
+});
+
+const telnumError = document.getElementById("telnumError");
+const telNumberInput = document.getElementById("user_number");
+telNumberInput.addEventListener("input", () => {
+  if (telNumberInput.value.trim() !== " ") {
+    telnumError.textContent = " ";
+  }
+});
+
+const emailidError = document.getElementById("emailidError");
+const emailIdInput = document.getElementById("user_emialId");
+emailIdInput.addEventListener("input", () => {
+  if (emailIdInput.value.trim() !== " ") {
+    emailidError.textContent = " ";
+  }
+});
+
+const validateForm = () => {
+  const name = document.getElementById("name").value.trim();
+  const telNumber = document.getElementById("user_number").value.trim();
+  const emailId = document.getElementById("user_emialId").value.trim();
+  const selectDate = document.getElementById("user_selectDate").value;
+  // const selectTime = document.getElementById("user_selectTime").value;
+  // const Guests = document.getElementById("guests").value;
+  // const indoor = document.getElementById("_indoor").checked;
+  // const outdoor = document.getElementById("_outdoor").checked;
+  // const noPreference = document.getElementById("no_prefer").checked;
+
+  const nameError = document.getElementById("nameError");
+  const telnumError = document.getElementById("telnumError");
+  const emailidError = document.getElementById("emailidError");
+
+  // Validaton starts from here
+
+  if (name === "") {
+    nameError.textContent = " Please Enter The Name ";
+    return false;
+  }
+
+  // const telNumberPattern = /^[0-9]{10}$/;
+  const telNumberPattern = /^[6-9][0-9]{9}$/;
+
+  if (!telNumberPattern.test(telNumber)) {
+    telnumError.textContent = " Please Enter a valid 10-digit Phone Number ";
+    return false;
+  }
+  telnumError.textContent = "";
+
+  const emailIdPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailIdPattern.test(emailId)) {
+    emailidError.textContent = " Enter Valid Email ID ";
+    return false;
+  }
+  return true;
+};
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   console.log(" Form Submitted");
+  // validateForm();
+
+  const isvalid = validateForm();
+
+  if (!isvalid) {
+    return;
+  }
+
+  const telnumError = document.getElementById("telnumError");
+  const emailidError = document.getElementById("emailidError");
 
   let reservationData = {
     name: document.getElementById("name").value,
@@ -52,15 +134,31 @@ form.addEventListener("submit", async (e) => {
     body: JSON.stringify(reservationData),
   });
 
+  const data = await response.json();
+
+  if (!response.ok) {
+    console.log(data.field);
+    console.log(data.message);
+    if (data.field === "telNumber") {
+      // console.log(telnumError);
+      telnumError.textContent = `${data.message}`;
+      return false;
+    } else if (data.field === "emailId") {
+      // console.log(telnumError);
+      emailidError.textContent = `${data.message}`;
+    }
+    return;
+  }
+
   if (response.ok) {
+    nameError.textContent = " ";
+    telnumError.textContent = "";
+    emailidError.textContent = "";
     successPopup.style.display = "flex";
 
     form.reset();
   }
 
-  const data = await response.json();
-
-  console.log(data.message);
   console.log(data.reservation);
 });
 
